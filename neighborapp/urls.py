4200e6dd161435ac07578bfe56a6fca7bd64fe60
Burence1 <burensomondi@gmail.com>
2021-06-03 21:07:47 +0300

    add neighborhood and business put and delete endpoints

--- neighborapp/urls.py
+++ neighborapp/urls.py
@@ -7,4 +7,6 @@ urlpatterns=[
   path('api/neighbors/',views.NeighborhoodList.as_view(),name='neighbor'),
   path('api/business/',views.BusinessList.as_view(),name='business'),
   path('api/users/',views.UserList.as_view(),name='users'),
+  path('api/users/update/<int:pk>/',views.UserList.as_view(),name='update_users'),
+  path('api/users/delete/<int:pk>/',views.UserList.as_view(),name='delete_users'),
 ]
